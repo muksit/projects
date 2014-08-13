72e55c945fb0c07f4c66433be04ef5f7ff5a09af
muksit <muksit@gmail.com>
2014-08-13 17:40:15 -0400

    fixed exceptions

--- tictactoe.py
+++ tictactoe.py
@@ -10,63 +10,57 @@ board = {
 		9 :' '
 		}
 
-def printboard():
+def print_board():
 	print board[1] +"|"+ board[2] +"|"+ board[3]
 	print board[4] +"|"+ board[5] +"|"+ board[6]
 	print board[7] +"|"+ board[8] +"|"+ board[9]
 
-def startgame():
+def start_game():
 	print "Here is what the board positions look like:"
 	print "1"  + "|" + "2"  + "|" + "3" 
 	print "4"  + "|" + "5"  + "|" + "6" 
 	print "7"  + "|" + "8"  + "|" + "9" 
 	for i in range(10):
 		if i%2 == 0:
-			askplayer1()
-			printboard()
-			if checkwin() == True:
+			ask_player1()
+			print_board()
+			if check_win() == True:
 				print "Win! Player 2 sucks!"
 				break
 		else:
-			askplayer2()
-			printboard()
-			if checkwin() == True:
+			ask_player2()
+			print_board()
+			if check_win() == True:
 				print "Win! Player 1 sucks!"
 				break
 
-
-def askplayer1():
+def ask_player1():
 	try:
-		player1move = int(raw_input("player 1, where do you want to draw your X?"))
-		if player1move in range(1,10) and board[player1move] == "_" or board[player1move] == " ":
-			board[player1move] = "X"
+		player1_move = int(raw_input("player 1, where do you want to draw your X?"))
+		if player1_move in range(1,10) and (board[player1_move] == "_" or board[player1_move] == " "):
+			board[player1_move] = "X"
 		else:
 			print "sorry that isn't a valid position please try again"
-			askplayer1()
+			ask_player1()
+
+	except ():
+		print "sorry that isn't a valid position please try again"
+		ask_player1()	
+
+def ask_player2():
+	try:
+		player2_move = int(raw_input("player 2, where do you want to draw your 0?"))
+		if player2_move in range(1,10) and (board[player2_move] == "_" or board[player2_move] == " "):
+			board[player2_move] = "0"
+		else:
+			print "sorry that isn't a valid position please try again"
+			ask_player2()
 
 	except (ValueError):
 		print "sorry that isn't a valid position please try again"
-		askplayer1()
+		ask_player2()
 
-	
-	
-
-def askplayer2():
-	try:
-		player2move = int(raw_input("player 2, where do you want to draw your 0?"))
-		if player2move in range(1,10)and board[player2move] == "_" or board[player2move] == " ":
-			board[player2move] = "0"
-		else:
-			print "sorry that isn't a valid position please try again"
-			askplayer2()
-
-	except (ValueError):
-		print "sorry that isn't a valid position please try again"
-		askplayer2()
-	
-
-
-def checkwin():
+def check_win():
 	if board[1] == board [2] == board [3] and board[1] != "_" and board[1] != " ":
 		return True
 	elif board[4] == board [5] == board [6] and board[4] != "_" and board[4] != " ":	
@@ -86,4 +80,5 @@ def checkwin():
 		 
 
 
-startgame()
+if __name__ == '__main__':
+   start_game()
