2ed06927a85c49a3e38d9edefa70ffb0b04c37ca
muksit <muksit@gmail.com>
2014-08-14 18:55:41 -0400

    adding new wordcloud project

---
+++ wordcloud/wordcloud.js
@@ -0,0 +1,94 @@
+$(function(){
+
+var fill = d3.scale.category20c();
+
+var blacklist = "a,we,We,table,It,Our,us,us.,The,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your"
+var splitblacklist = blacklist.split(",")
+var phrases = "very, so, not"
+var sampletext = "I am a huge fan of this steak house. I came here after my gf and I went to Cirque du Soleil..down the street (which I totally recommend, but that's a different review). I'm sad that we didn't start out with an appetizer, but the bread that they gave us was enough to hold us down till our entrees came out. I feel like I'd be pretty excited about getting free bread even if it was the white Wonder bread from my childhood... I ordered the Cowboy Ribeye 20oz. It was cooked perfectly with a pink center. With every bite I couldn't wait to put another piece into my mouth. BUT even better than my steak was what my gf ordered: Australian Lamb Chops. WOW...i will def. be ordering the Lamb Chops next time. It was almost like I didn't even need a knife to cut the meat. For sides we shared the Garlic Mashed Potatoes and the Crispy Brussel Sprouts w/ spicy mustard yogurt sauce. Mashed potatoes were good, but nothing great...they were what we ordered Garlic Mashed Potatoes. The Brussel Sproutes were unreal. They were cooked great, but the yogurt sauce on top made it an awesome side...I highly recommend ordering the brussel sprouts. I also had the Jefferson Old Fashioned, which was very tasty. We will be back. We were at Gaylord National for a conference and a portion of our group came here for dinner the first night. I have to say that the staff was very accommodating to us, especially when people continued to trickle to our table after others had already ordered and eaten. They helped pull up chairs to the table and promptly came around and took orders. That kind of service is hard to come by, especially in upscale steakhouses like this. On to the food. Delicious. Exactly what you would expect from a restaurant of this caliber. HUGE portion of meat, but be aware that ALL sides come separately at an additional cost. To me a steakhouse is a steakhouse. If you have been to enough of them they all kinda start to blend in together. So a great steakhouse has to make itself standout in other ways. Old hickory stands out for a few reasons: 1) Cheese Diva 2) Wall street Mule 3) Cobb salad 4) Views The staff was entertaining, and I love the cheese diva. She had a charming personality and great picks on cheese. I am big cocktail person. Creative cocktails make a good steakhouse great. I am a big fan of a moscow mule, and while OH has &#x0022;Wall Street Mule&#x0022; which is their own twist it's a great variation on the original drink. The cobb salad is next and all I have to say is one word: BACON. I don;t even know if you can call it a salad it should be called heaven. Thick chunks of what i would say may be pieces of pork belly versus traditional bacon cover the wedge with almost powdered egg and blue cheese. Again, HEAVEN. Last, come the views. Table #110 and #114 are the perfect date night setting. They are the same table on opposite sides on the dining room that both basically have a private view of the potomac. If you set your reservation to be eating around 8:30 you get the most wonderful view of the sun setting over the potomac. BONUS: Also, if you dine at Old Hickory you have access to their roof top bar at the Gaylord for after dinner drinks which offer an amazing view of the potomac, old town, and DC. If you like my review follow me on instagram and Facebook for all the awesome places to nom in the DC area: NomNoms4Foodies This is a great place to come for a nice, fancy night out. The ambiance was great and our waiter was friendly and helpful. Between us, my boyfriend and I had the mussels for a starter and shared the porterhouse and lamb chops. Out of the three, the lamb chops was the best. The porterhouse was good, just nothing particularly special in my book. We had a fantastic meal here. Let me say it another way: there were five of us, and we all had an amazing meal. Our waiter, however, was terrible. If you've ever spent time in National Harbor, you know that everyone who works there is amazingly courteous, almost to excess... but they know how to treat customers. And at Old Hickory, this is no exception... for everyone but our waiter. Our guy was rude from the get-go, got drink orders wrong (more than once), and totally missed the order on a few items. It wasn't terribly busy, and I'll always give a pass on messing up an order now and then; but to have a lousy attitude from the beginning of the meal to the end is just wrong. If you're going to charge $100+ per person for a meal, you cannot allow people like this to represent your business. That being said, the drink stewards, bussers, bartenders, and other wait staff were great. And chances are, if you dine here, you won't get stuck with this guy. I'd eat there again, I'd just be sure to request someone else. My husband and I came here while a development convention was in town at the Gaylord. I must say this was pretty good. I gave it four stars because my cowboy ribeye was ho hum, but his New York strip with May tag cheese was out of this world. The cheese selection was awesome as well - a must do. If you do wine? Ask for it by the glass, not the bottle - a good waiter will keep topping you off like ours did and you won't need a bottle. Maybe bottles are just needed for large parties. We came here on a Saturday night looking for a great experience, and were not disappointed. After being seated for an 8:30 reservation, we were promptly greeted by Steve, who was fantastic - not only in his knowledge of the menu, but his knowledge of the area. We &#x0022;picked his brain&#x0022; about some places near Camden Yards, as we are returning in a couple of months for a baseball trip with kids, and the advice was great - he even wrote some spots to try on a ticket for us. But, to get back to the food, we started the evening with the artisanal cheeses. The Maitre d' Fromage went through the selections and she knew her stuff - I hate I cannot remember her name, but she was great. Also, when the bread came out, we discovered that the butter spreads could be a food group all on their own. The whipped honey butter was so simple, but was insanely delicious. For entrees, we chose the New York Strip and the Filet of Beef with lobster tail- rare and medium respectively. Both cuts of beef were out of this world and fantastic. The flavor is something we can't put into a review, you just have to try it. For our sides, we chose the garlic mashed potatoes and the crispy brussel sprouts, mustard yogurt sauce. Again neither disappointed. Our ONLY drawback to the meal was the lobster tail that came with the meal - it was undercooked. However, Steve did offer to have the chef fire it again and he did take it off the bill, so he completely made good with it - it happens. OK...the best part....the Roasted Marshmallow Shake. Oh My. Lord. The chef has won awards from this thing and I can see why. It is made with ice cream, roasted marshmallow, and, wait for it.....Woodford Reserve Bourbon. WOW!! It was completely decadent. The bourbon in the shake got us talking with Steve about select bourbons and he even brought out a sampling of 1792 Ridgemont Reserve for us to try....let me put it like this - we have his schedule, and will be asking for him WHEN we return. He even recommended a fantastic Beringer Private Reserve Chardonnay that we would have never chosen; however, he assured us of the quality and to our surprise it went great with dinner So......in conclusion, do not let the pricing or occasion bad review scare you away. If you are appreciative of great food and wine, and impeccable service, you should pay them a visit. Very impressive food, service and ambiance. Me and the Fiance came to celebrate a night out to ourselves with a late night dinner and Old Hickory delivered. After finding the restaurant (if I could complain about anything it would be directions once inside the Gaylord) we were greeted and taken to our seat, ask for table 111. Our table was a corner table that overlooked the Harbor and Potomac River. Our server, Happy, was very knowledgeable and attentive. The bread is delivered with honey butter and a onion chive dip, both spectacular. We order the table side cheese sampler as the appetizer which was quite an experience and recommended. It takes the idea of a cheese sampler to knew meaning. The wagyu NY strip was a little fattier than I expected but still tasty. I expected better for the price. The fiance's filet with crab and asparagus toppings was also cooked to perfection and exceptional. I think we filled up to much on the bread and butter because we weren't able to finish the steaks. We got garlic mashed potatoes (amazing) and brussel sprouts as our sides. If I could do it again, I would like to try the steak tartar appetizer, order a different steak than the wagyu NY strip and get a different side than the brussel sprouts. Enjoyed a great dinner with great service. Our server was back and forth checking on us and never an empty wine glass on our table. Will visit again when I'm back in DC. I went to this place on a first date and I must say I was really impressed. Our table was a corner table that overlooked the Harbor and Potomac River. Our server was very knowledgeable and attentive. She was also a pleasure as she shared a few laughs with us. The bread is delivered with honey butter and a onion chive dip, both spectacular. I ordered the filet (medium) with crab and asparagus toppings which was delectable and cooked to perfection. My date order the ribeye(medium rare) which was also cooked to perfection. I think we filled up to much on the bread and butter because we weren't able to finish the steaks. We got garlic mashed potatoes (amazing) and spinach as our sides. Overall, I highly recommend this place for the food, ambiance, and service. Took a group there for dinner at a conference last night. The service was great and the food was really well done. I could have eaten many bowls of the French onion soup. Steak was priced about what is expect for a place like this. The only thing that shocked me was the bar tab. Standard well drinks were very spendy in my book. But overall a great experience. We loved the cheese presentation as an appetizer. Delicious brined pork chop, and even better filet Oscar! Roasted Brussels sprouts were just ok. Vanilla flan was unique and just the right sweet touch at the end. Fantastic service! Best of all was the table location and time of day. We lucked into a window table overlooking the Potomac, just as the sun was setting!! Absolutely gorgeous!! If you can manage the timing, this setting is spectacular!!"
+var splitwords = sampletext.split(" ");
+var wordfrequency = getFrequency(sampletext);
+var keys = [];
+keys = Object.keys(wordfrequency);
+
+var filteredlist = filter(keys)
+
+
+function filter(list){
+  var newlist = [];
+  for (var x=0; x < list.length; x++) {
+      if (containsObject(list[x], splitblacklist) == true) {
+        newlist.pop(list[x])
+      }
+      else{
+        newlist.push(list[x])
+      }
+    }
+   return newlist   
+
+}
+
+
+function containsObject(obj, list) {    
+    for ( var i = 0; i < list.length; i++) {
+        if (list[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+
+  d3.layout.cloud().size([2000, 2000])
+      .words(filteredlist.map(function(d) {
+      return {text: d, size: 15 * (getFrequency(sampletext)[d])+5 };
+      }))
+      .rotate(function() { return ~~(Math.random() * 2) * 90; })
+      .font("Impact")
+      .fontSize(function(d) { return d.size; })
+      .on("end", draw)
+      .start();
+
+
+
+function getFrequency(text) {
+  var freq = {};
+  var arraytext = text.split(" ");
+  for (var i=0; i < arraytext.length; i++) {
+    var word = arraytext[i];      
+      if (freq[word]) {
+        freq[word]++;
+      } 
+      else {
+         freq[word] = 1;
+      }
+  }
+
+  return freq;
+};
+
+console.log(getFrequency(sampletext).the);
+
+
+  function draw(words) {
+    d3.select("body").append("svg")
+        .attr("width", 2000)
+        .attr("height", 2000)
+      .append("g")
+        .attr("transform", "translate(850,650)")
+      .selectAll("text")
+        .data(words)
+      .enter().append("text")
+        .style("font-size", function(d) { return d.size + "px"; })
+        .style("font-family", "Impact")
+        .style("fill", function(d, i) { return fill(i); })
+        .attr("text-anchor", "middle")
+        .attr("transform", function(d) {
+          return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+        })
+        .text(function(d) { return d.text; });
+  };
+  
+
+});
