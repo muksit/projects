a0c0093827c24bdf395f471c9c51d664c6f53773
Muksit <muksit@gmail.com>
2014-08-22 03:38:10 -0400

    app is now running on flask

---
+++ wordcloud/static/wordcloud.js
@@ -0,0 +1,103 @@
+$(function(){
+
+var fill = d3.scale.category20c();
+
+
+var phrases = "very, so, not"
+
+request = $.get("/ask", {word: "gaza"}, function(data){
+    var sampletext = data;
+    var splittext = sampletext.split(" ");
+
+    d3.layout.cloud().size([2000, 2000])
+        .words(splittext.map(function(d) {
+        return {text: d, size: 5 * (getFrequency(sampletext)[d])+5 };
+        }))
+        .rotate(function() { return ~~(Math.random() * 2) * 90; })
+        .font("Impact")
+        .fontSize(function(d) { return d.size; })
+        .on("end", draw)
+        .start();
+
+    var wordfrequency = getFrequency(sampletext);
+    var keys = [];
+    keys = Object.keys(wordfrequency);
+
+    function draw(words) {
+      d3.select("body").append("svg")
+          .attr("width", 2000)
+          .attr("height", 2000)
+        .append("g")
+          .attr("transform", "translate(850,650)")
+        .selectAll("text")
+          .data(words)
+        .enter().append("text")
+          .style("font-size", function(d) { return d.size + "px"; })
+          .style("font-family", "Impact")
+          .style("fill", function(d, i) { return fill(i); })
+          .attr("text-anchor", "middle")
+          .attr("transform", function(d) {
+            return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+          })
+          .text(function(d) { return d.text; });
+    };
+
+
+  
+  });
+
+
+
+
+/*var filteredlist = filter(keys)
+
+
+function filter(list){
+  var newlist = [];
+  for (var x=0; x < list.length; x++) {
+      if (containsObject(list[x], splitblacklist) == true) {
+        newlist.pop(list[x])
+      }
+      else{
+        newlist.push(list[x])
+      }
+    }
+   return newlist }  */
+
+
+
+
+/*function containsObject(obj, list) {    
+    for ( var i = 0; i < list.length; i++) {
+        if (list[i] === obj) {
+            return true;
+        }
+    }
+    return false;
+}*/
+
+
+
+  function getFrequency(text) {
+    var freq = {};
+    var arraytext = text.split(" ");
+    for (var i=0; i < arraytext.length; i++) {
+      var word = arraytext[i];      
+        if (freq[word]) {
+          freq[word]++;
+        } 
+        else {
+           freq[word] = 1;
+        }
+    }
+
+    return freq;
+  };
+
+
+
+
+  
+  
+
+});
--- wordcloud/twitterapp.py
+++ wordcloud/twitterapp.py
@@ -1,28 +1,53 @@
 from TwitterAPI import TwitterAPI
 import nltk
+from collections import Counter
+import os, time, json, datetime 
+from json import JSONEncoder, load
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash, json, jsonify, send_file
 
 
-
+# create application 
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config["DEBUG"] = True  # Only include this while you are testing your app
 
 key = "8PN0z0v104Kvos7z2xhETftQQ"
 secret = "h5UxoOSUul8pWQ1SjpsIPYBZgIAjjwPvDd9bnyhgNhidFPAZbq"
 token = "27931231-gtOLN2K1A1NpJG69OjqBtvMYxW4ltqUlkLY2ydQN0"
 token_secret = "N8zPKf89ZhBLrAkRnlrCeza1NpnAFuvwuqRDMquD0Vo6C"
+blacklist = "a,we,We,table,It,Our,us,us.,/,/,The,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your"
+splitblacklist = blacklist.split()
 
 api = TwitterAPI(key, secret, token, token_secret)
 
-r = api.request('search/tweets', {'q':'gaza', 'lang': 'en', 'count': 5})
-# for item in r.get_iterator():
-textarray = []
-for item in r:
-    
-    textarray.append(item['text'])
 
-print textarray
 
-blacklist = "a,we,We,table,It,Our,us,us.,/,/,The,able,about,across,after,all,almost,also,am,among,an,and,any,are,as,at,be,because,been,but,by,can,cannot,could,dear,did,do,does,either,else,ever,every,for,from,get,got,had,has,have,he,her,hers,him,his,how,however,i,if,in,into,is,it,its,just,least,let,like,likely,may,me,might,most,must,my,neither,no,nor,not,of,off,often,on,only,or,other,our,own,rather,said,say,says,she,should,since,so,some,than,that,the,their,them,then,there,these,they,this,tis,to,too,twas,us,wants,was,we,were,what,when,where,which,while,who,whom,why,will,with,would,yet,you,your"
-splitblacklist = blacklist.split()
-print splitblacklist
+@app.route('/',)
+def input():
+    return render_template('index.html')
+
+
+#return list of words
+@app.route('/ask')
+def givecloud():
+    wordtograph = request.args.get('word')
+    print wordtograph
+    words = ', '.join(getwordlist(wordtograph))
+    return removedupes(words)
+
+
+# get a clean list of words 
+def getwordlist(word):
+    r = api.request('search/tweets', {'q': word, 'lang': 'en', 'count': 100})
+    textarray = []
+    for item in r:
+        textarray.append(item['text'])
+    filteredwords = wordfilter(textarray)
+    tokenizedfilteredwords = filteredwords.split()
+    return tokenizedfilteredwords
+
+
 
 def containsObject(obj, list2):
     for i in list2:
@@ -33,15 +58,11 @@ def containsObject(obj, list2):
 def wordfilter(list2):
     newlist = ""
     for x in list2:
-        if (containsObject(x, splitblacklist) == False):
+        if (containsObject(x, splitblacklist) == False) and (x[:4] != "http") and (x[:2] != "RT"):
             newlist = newlist + x
+            print x[:4]
         # if (adverb(list[x], ))
     return newlist   
-
-
-filteredwords = wordfilter(textarray)
-tokenizedfilteredwords = filteredwords.split()
-print tokenizedfilteredwords
 
 
 def containsObject(obj, list):
@@ -49,3 +70,29 @@ def containsObject(obj, list):
         if (i == obj):
             return True
     return False
+
+def removedupes(text):
+    wordfrequency = getFrequency(text)
+    print wordfrequency
+    keys = []
+    for item in wordfrequency:
+        keys += item.key
+    
+    print keys
+  
+
+def getFrequency(text):
+    freq = {};
+    arraytext = text.split();
+    print arraytext
+    freq = Counter(arraytext)
+    print freq
+    return freq.keys
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
